f29e0ee77ac6c7ac1766f3c4eeba8c7b7ee0fe83
kirill.tarasevich <kirill.tarasevich@gmail.com>
2020-07-02 15:32:07 +0300

    commit

--- Tailspin.SpaceGame.Web/Models/ErrorViewModel.cs
+++ Tailspin.SpaceGame.Web/Models/ErrorViewModel.cs
@@ -2,8 +2,9 @@ namespace TailSpin.SpaceGame.Web.Models
 {
     public class ErrorViewModel
     {
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
         public string RequestId { get; set; }
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        
     }
 }
